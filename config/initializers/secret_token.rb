a6a8e2e0018bfec9e9bcf3a4151dcc61758be1c6
dempsey <dempseyaccount@hotmail.com>
2013-06-02 00:04:42 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vereniging::Application.config.secret_token = '04c270dc715499e9b1f2c033eb95d7a6b075e110fb5752f061cf9ef45360a227480954dd3866305925d6d5cb98416d83070878521b1ac10bda6063bb10d551ad'
