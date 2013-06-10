d9a588e02c81fc6a3509db3c140de6d649b6dee0
dempseyaccount <dempseyaccount@hotmail.com>
2013-06-10 17:25:53 +0300

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,17 @@
+
 module ApplicationHelper
+
   def flash_class(type)
-    case type
-    when :alert
-      "alert_error"
-    when :notice
-      "alert_success"
-    else
-      ""
-    end
-  end
+		case type
+		when :alert
+			"alert-error"
+		when :notice
+			"alert-success"
+  when :info
+      "alert-info"
+		else
+			""
+		end
+	end
 end
+
